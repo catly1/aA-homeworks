36959b0478260cded8919a807cbcd6e1126bb7eb
Carlos Catly <ccatly@gmail.com>
2019-09-29 21:42:00 -0700

    at the mongodb part

--- W19D1/GraphQLHW/index.js
+++ W19D1/GraphQLHW/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Goodbye Worlds"));
+app.use(bodyParser.json());
 app.listen(5000, () => console.log('Server is running on port 5000'));
