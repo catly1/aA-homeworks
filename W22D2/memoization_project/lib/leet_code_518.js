b795c2134a08bf5651ac0d787da493d372aa6848
Carlos Catly <ccatly@gmail.com>
2019-10-22 15:06:22 -0700

    did the memoization project

---
+++ W22D2/memoization_project/lib/leet_code_518.js
@@ -0,0 +1,21 @@
+// Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
+// Feel free to use this file for scratch work.
+
+var change = function (amount, coins) {
+
+
+        let key = amount + '-' + coins;
+        if (key in memo) return memo[key];
+        if (amount === 0) return 1;
+
+        let currentCoin = coins[coins.length - 1];
+        let total = 0;
+
+        for (let qty =; qty * currentCoin <= amount; qty++){
+            total += change(amount - qty * currentCoin, coins.slice(0, -1), memo);
+        }
+
+        memo[key] = total;
+        return memo[key];
+
+};
